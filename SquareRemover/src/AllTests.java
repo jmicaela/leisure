f0fe64495a732ac85efc78b4529ea778649f7541
jmicaela <jmicaela8@gmail.com>
2020-01-31 20:34:02 -0500

    Removed common-lang dependency

--- SquareRemover/src/AllTests.java
+++ SquareRemover/src/AllTests.java
@@ -2,8 +2,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({ SquareRemoverTester.class, SmartPlayerTester.class })
-public class AllTests {
-
-}
+/*
+ * @RunWith(Suite.class)
+ * 
+ * @SuiteClasses({ SquareRemoverTester.class, SmartPlayerTester.class }) public
+ * class AllTests {
+ * 
+ * }
+ */
